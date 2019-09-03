2ca5dd9332442ef410979ba202fd68ab8fff7827
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2019-09-03 08:40:15 -0500

    created item schema

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 //Connect to mongo
-
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+// Port app is servered on
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
